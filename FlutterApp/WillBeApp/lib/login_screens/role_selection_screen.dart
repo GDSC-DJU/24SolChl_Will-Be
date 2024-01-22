12e208e1775729ce8258a68c92ca7aa5c41a0ae0
기홍 <gihong0409@naver.com>
2024-01-22 16:32:41 +0900

    FIX : Now you can build it 빌드가 안되던 문제를 해결했지만 Firestore관련 코드는 모두 지움.
    ** ** :

--- FlutterApp/WillBeApp/lib/login_screens/role_selection_screen.dart
+++ FlutterApp/WillBeApp/lib/login_screens/role_selection_screen.dart
@@ -2,16 +2,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:solution/login_screens/submit_profile_screen.dart';
-import 'package:solution/user_controller.dart';
 
 class RoleSelectionScreen extends StatelessWidget {
   const RoleSelectionScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    UserController userController = Get.find();
-    User user = userController.user;
-
     return Scaffold(
       body: Expanded(
         child: Center(
@@ -19,9 +15,9 @@ class RoleSelectionScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               ElevatedButton(
-                child: Text(
-                  user.email!.toString(),
-                  style: const TextStyle(fontSize: 20),
+                child: const Text(
+                  "유저의 이메일",
+                  style: TextStyle(fontSize: 20),
                 ),
                 onPressed: () {
                   Navigator.push(
