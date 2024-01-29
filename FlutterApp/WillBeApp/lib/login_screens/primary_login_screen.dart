a1f67b57cba5ec22dbd71977b95d0f443e56cd93
기홍 <gihong0409@naver.com>
2024-01-29 10:13:18 +0900

    Merge of test branch

--- FlutterApp/WillBeApp/lib/login_screens/primary_login_screen.dart
+++ FlutterApp/WillBeApp/lib/login_screens/primary_login_screen.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:solution/login_screens/role_selection_screen.dart';
-import 'package:solution/main_page/main_page.dart';
+import 'package:solution/main_feat_screens/main_page.dart';
 
 class PrimaryLoginScreen extends StatefulWidget {
   const PrimaryLoginScreen({super.key});
