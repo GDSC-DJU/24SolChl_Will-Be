e550d7fb209be496e5848ace9488e6e21eab6714
기택 <samscientist@naver.com>
2024-02-21 18:19:33 +0900

    Add `SendRequest` class for API POST requesting

--- FlutterApp/WillBeApp/lib/reporting/send_request.dart
+++ FlutterApp/WillBeApp/lib/reporting/send_request.dart
@@ -1,18 +1,36 @@
+import 'dart:convert';
+import 'dart:developer';
 import 'package:http/http.dart' as http;
 
-Future<void> _sendPostRequest(
-    String body, String? _responseBody) async {
-  // HTTP POST 요청 송신 및 응답 수신 메소드
-  try {
-    final response = await http.post(
-      // HTTP POST 요청 송신 및 응답 수신
-      Uri.parse('https://willbe-nlp-t5feuxbhta-du.a.run.appv1/summary/report'), // URL을 파싱
-      headers: {"Content-Type": "application/json"}, // 헤더 설정
-      body: body, // 요청 본문 설정
-    );
-    // 상태 변경
-    _responseBody = response.body; // 응답 본문 저장
-  } catch (e) {
-    print(e);
+class SendRequest {
+  SendRequest({
+    required this.url,
+    required this.inputBody,
+  }) {
+    _inputJson = jsonEncode(inputBody);
+    _sendPostRequest();
+  }
+
+  final String url;
+  final List<dynamic> inputBody;
+  late final String _inputJson;  // JSON format body
+  String? _responseJson;
+
+  String? get responseJson => _responseJson;
+
+  Future<void> _sendPostRequest() async {
+    // HTTP POST 요청 송신 및 응답 수신 메소드
+    try {
+      final response = await http.post(
+        // HTTP POST 요청 송신 및 응답 수신
+        Uri.parse(url), // URL을 파싱
+        headers: {"Content-Type": "application/json"}, // 헤더 설정
+        body: _inputJson, // 요청 본문 설정
+      );
+      // 상태 변경
+      _responseJson = response.body; // 응답 본문 저장
+    } catch (e) {
+      log(e.toString());
+    }
   }
 }
