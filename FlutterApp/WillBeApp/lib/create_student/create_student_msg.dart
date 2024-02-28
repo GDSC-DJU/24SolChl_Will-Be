78a2b01e98a87a35cfb1729f54cfbb48393dd82e
기홍 <gihong0409@naver.com>
2024-02-28 22:03:40 +0900

    `FIX` : Widget error fIx
    <본문>
    Expanded 위젯의 잘못된 사용으로 로그인이 안되던 문제 해결

--- FlutterApp/WillBeApp/lib/create_student/create_student_msg.dart
+++ FlutterApp/WillBeApp/lib/create_student/create_student_msg.dart
@@ -33,68 +33,72 @@ class _Create_student_Msg_State extends State<Create_student_Msg> {
       child: Scaffold(
         resizeToAvoidBottomInset: false,
         appBar: AppBar(),
-        body: Expanded(
-          child: Container(
-            child: Column(
-              children: [
-                SizedBox(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.03,
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.03,
+                          ),
+                          SizedBox(
+                            width: 150,
+                            child: Image.asset(
+                              'assets/icons/appicon/willbe-icon-darker.png',
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.02,
+                          ),
+                          Text(
+                            "Will Be",
+                            style: Theme.of(context).textTheme.headlineLarge,
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.02,
+                          ),
+                          const Text(
+                            "서비스를 이용하기 위해서는\n\n아이와 아이의 도전행동을 등록해야해요\n\n루틴을 등록하면\n해당시간대의 환경이\n자동으로 기록돼요\n\n기록하고 리포트를 받아보세요\n\n의사소통사전을 통해\n아이가 무엇을 표현하고자 하는지\n알아보세요",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 15),
+                          ),
+                        ],
                       ),
-                      SizedBox(
-                        width: 150,
-                        child: Image.asset(
-                          'assets/icons/appicon/willbe-icon-darker.png',
-                          fit: BoxFit.contain,
+                    ),
+                    const Spacer(),
+                    ElevatedButton(
+                      onPressed: () {
+                        _submitData();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero,
+                        ),
+                        padding: EdgeInsets.zero,
+                      ),
+                      child: Container(
+                        width: double.infinity,
+                        padding: const EdgeInsets.symmetric(vertical: 32),
+                        color: const Color.fromARGB(255, 22, 72, 99),
+                        child: const Center(
+                          child: Text(
+                            '시작하기',
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          ),
                         ),
                       ),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.02,
-                      ),
-                      Text(
-                        "Will Be",
-                        style: Theme.of(context).textTheme.headlineLarge,
-                      ),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.02,
-                      ),
-                      const Text(
-                        "서비스를 이용하기 위해서는\n\n아이와 아이의 도전행동을 등록해야해요\n\n루틴을 등록하면\n해당시간대의 환경이\n자동으로 기록돼요\n\n기록하고 리포트를 받아보세요\n\n의사소통사전을 통해\n아이가 무엇을 표현하고자 하는지\n알아보세요",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 15),
-                      ),
-                    ],
-                  ),
-                ),
-                const Spacer(),
-                ElevatedButton(
-                  onPressed: () {
-                    _submitData();
-                  },
-                  style: ElevatedButton.styleFrom(
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.zero,
                     ),
-                    padding: EdgeInsets.zero,
-                  ),
-                  child: Container(
-                    width: double.infinity,
-                    padding: const EdgeInsets.symmetric(vertical: 32),
-                    color: Color.fromARGB(255, 22, 72, 99),
-                    child: const Center(
-                      child: Text(
-                        '시작하기',
-                        style: TextStyle(fontSize: 20, color: Colors.white),
-                      ),
-                    ),
-                  ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
